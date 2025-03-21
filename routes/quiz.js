5626903b9d1fd1585f933573b9d136043c902d80
alex <eafo72@gmail.com>
2025-03-21 16:11:31 -0600

    endpoint quiz

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,15 +1,8 @@
 /* Importing the express module and creating an instance of it. */
 const express = require("express");
 const app = express.Router();
-const bcryptjs = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const auth = require("../middlewares/authorization");
 const db = require("../config/db");
-const mailer = require("../controller/mailController");
-const cloudinary = require("../config/cloudinaryConfig");
-const bodyParser = require("body-parser");
 
-app.use(bodyParser.json({ limit: "10mb" })); // Permitir imágenes grandes en base64
 
 //preguntas por id trivia
 app.get("/quiz/:idTrivia", async (req, res) => {
@@ -27,7 +20,7 @@ app.get("/quiz/:idTrivia", async (req, res) => {
   
   try {
 
-    let query = `SELECT * FROM preguntas WHERE id_preguta >= ? LIMIT 3`;
+    let query = `SELECT * FROM preguntas WHERE id_pregunta >= ? LIMIT 3`;
     let trivias = await db.pool.query(query, [questionId]);
     trivias = trivias[0];
 
