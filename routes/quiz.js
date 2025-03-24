87c9cdd52e17d936cbb1bd5882efc1bc4b723330
alex <eafo72@gmail.com>
2025-03-24 13:47:53 -0600

    endpoint quiz error

--- routes/quiz.js
+++ routes/quiz.js
@@ -32,6 +32,8 @@ app.get("/quiz/:idTrivia/:idUser", async (req, res) => {
     LIMIT 1
     `;
 
+    console.log(query);
+
     let quiz = await db.pool.query(query, [startQuestionId, endQuestionId, id_usuario]);
     quiz = quiz[0];
 
