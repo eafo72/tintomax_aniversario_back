0f352f71973f96ce301cdde9c308873c47a24450
alex <eafo72@gmail.com>
2025-03-31 18:32:35 -0600

    new quiz result endpoint

--- routes/quiz.js
+++ routes/quiz.js
@@ -107,16 +107,14 @@ app.get("/result/:idTrivia/:idUser", async (req, res) => {
     //pregunta 1
     query = `SELECT * FROM preguntas INNER JOIN respuestas ON preguntas.id_pregunta = respuestas.id_preg_resp WHERE preguntas.id_pregunta = ? AND respuestas.id_usuario_resp = ?`;
     let resultado1 = await db.pool.query(query, [id_pregunta1, id_usuario]);
-    resultado1 = resultado1[0][0];
     resultado1.forEach(row => quiz.push(row));
     
 
     //pregunta 2
     query = `SELECT * FROM preguntas INNER JOIN respuestas ON preguntas.id_pregunta = respuestas.id_preg_resp WHERE preguntas.id_pregunta = ? AND respuestas.id_usuario_resp = ?`;
     let resultado2 = await db.pool.query(query, [id_pregunta2, id_usuario]);
-    resultado2 = resultado2[0][0];
     resultado2.forEach(row => quiz.push(row));
-    
+
        
     res.status(200).json({ error: false, quiz });
 
