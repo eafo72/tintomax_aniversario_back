eeac9616f957f82e226b58f1bc9d050b8a90b457
alex <eafo72@gmail.com>
2025-04-03 16:28:31 -0600

    suma de puntos del usuario

--- routes/answers.js
+++ routes/answers.js
@@ -96,6 +96,13 @@ app.get("/check/:idTrivia/:idUser/:idQuiz/:idAnswer/:numQuestion", async (req, r
 
         }
 
+        //agregar los puntos ganados al usuario
+        if(totalPuntos > 0){
+          const updateQuery = `UPDATE usuarios SET acumulado_usur = acumulado_usur + ? WHERE id_usuario = ?`;
+          await db.pool.query(updateQuery, [totalPuntos,id_usuario]);
+        }
+
+
 
     res.status(200).json({ error: false, resultado, totalPuntos });
     
