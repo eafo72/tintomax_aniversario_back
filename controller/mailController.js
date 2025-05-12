e4c9114227065b2efa5cf53ed6be42b1cc61314f
eafo72 <eafo72@gmail.com>
2025-05-12 12:58:24 -0600

    implementar aws SES

--- controller/mailController.js
+++ controller/mailController.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 require('dotenv').config()
-
+/*
 let transporter = nodemailer.createTransport({
     host: process.env.MAILHOST,
     port: 587,
@@ -10,7 +10,17 @@ let transporter = nodemailer.createTransport({
         pass: process.env.PASSMAIL, // generated ethereal password
     },
 });
+*/
 
+const transporter = nodemailer.createTransport({
+  host: 'email-smtp.us-east-2.amazonaws.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.SES_USER,
+    pass: process.env.SES_PASSWORD
+  }
+});
 // send mail with defined transport object
 //let info = await transporter.sendMail(message);
 
